5573a002a4696595d696950464c8fdca6c901256
Samurais <hailiang.hl.wang@gmail.com>
2014-08-23 12:36:39 +0800

    #107 add bootstrap theme and a simple tool bar

--- www/js/index.js
+++ www/js/index.js
@@ -45,6 +45,6 @@ var app = {
     // Update DOM on a Received Event
     receivedEvent: function(id) {
         console.log('Received Event: ' + id);
-        connMap();
+        //connMap();
     }
 };
