791b223a6cebbdb8d124607e0fc779b274a25ab5
Samurais <hailiang.hl.wang@gmail.com>
2014-08-24 08:38:53 +0800

    #110 add persist navbar in footer and custom theme

--- www/js/index.js
+++ www/js/index.js
@@ -47,11 +47,29 @@ function fixedFooter(){
 }
 
 function setUp(){
-    $(document).ready(function(){
-        fixedFooter();
-        createMap();
-        createHomeSwiperHeader();
+    $(function() {
+        $( "[data-role='navbar']" ).navbar();
+        $( "[data-role='header'], [data-role='footer']" ).toolbar();
     });
+    // Update the contents of the toolbars
+    $( document ).on( "pagecontainershow", function() {
+        // Each of the four pages in this demo has a data-title attribute
+        // which value is equal to the text of the nav button
+        // For example, on first page: <div data-role="page" data-title="Info">
+        var current = $( ".ui-page-active" ).jqmData( "title" );
+        // Change the heading
+        $( "[data-role='header'] h1" ).text( current );
+        // Remove active class from nav buttons
+        $( "[data-role='navbar'] a.ui-btn-active" ).removeClass( "ui-btn-active" );
+        // Add active class to current nav button
+        $( "[data-role='navbar'] a" ).each(function() {
+            if ( $( this ).text() === current ) {
+                $( this ).addClass( "ui-btn-active" );
+            }
+        });
+    });
+    createMap();
+    createHomeSwiperHeader();
 }
 
 var app = {
