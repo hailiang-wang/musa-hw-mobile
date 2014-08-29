7c78a3d73dab2a8f30fc213e69d534c39f12c722
Samurais <hailiang.hl.wang@gmail.com>
2014-08-29 20:26:53 +0800

    #120 create sildes from a function

--- www/swiper/assets/messages.js
+++ www/swiper/assets/messages.js
@@ -1,6 +1,34 @@
 
+/* format string value with arguments */
+String.prototype.format = String.prototype.f = function() {
+    var s = this,
+        i = arguments.length;
 
-function createMessageSlides(){
+    while (i--) {
+        s = s.replace(new RegExp('\\{' + i + '\\}', 'gm'), arguments[i]);
+    }
+    return s;
+};
+
+/* if a string ends with a given suffix */
+String.prototype.endsWith = function(suffix) {
+    return this.indexOf(suffix, this.length - suffix.length) !== -1;
+};
+
+/* if a string starts with a given prefix */
+String.prototype.startsWith = function (str){
+    return this.indexOf(str) == 0;
+};
+
+
+function getSlide(el){
+  return slide = '<div class="swiper-slide {0}-slide">'.f(el.style)
+      +  '<div class="title">{0}</div>'.f(el.title)
+      + '</div>';
+
+}
+
+function createMessageSwiper(){
   var holdPosition = 0;
   var mySwiper = new Swiper('.swiper-container',{
     slidesPerView:'auto',
@@ -27,36 +55,46 @@ function createMessageSlides(){
         loadNewSlides();
       }
     }
-  })
+  });
   var slideNumber = 0;
   function loadNewSlides() {
-    /* 
-    Probably you should do some Ajax Request here
-    But we will just use setTimeout
-    */
-    setTimeout(function(){
-      //Prepend new slide
-      var colors = ['red','blue','green','orange','pink'];
-      var color = colors[Math.floor(Math.random()*colors.length)];
-      mySwiper.prependSlide('<div class="title">New slide '+slideNumber+'</div>', 'swiper-slide '+color+'-slide')
+      /* 
+      Probably you should do some Ajax Request here
+      But we will just use setTimeout
+      */
+      // #TODO read data from server
+      setTimeout(function(){
+        //Prepend new slide
+        var colors = ['red','blue','green','orange','pink'];
+        var color = colors[Math.floor(Math.random()*colors.length)];
+        mySwiper.prependSlide('<div class="title">New slide '+slideNumber+'</div>', 'swiper-slide '+color+'-slide')
 
-      //Release interactions and set wrapper
-      mySwiper.setWrapperTranslate(0,0,0)
-      mySwiper.params.onlyExternal=false;
+        //Release interactions and set wrapper
+        mySwiper.setWrapperTranslate(0,0,0)
+        mySwiper.params.onlyExternal=false;
 
-      //Update active slide
-      mySwiper.updateActiveSlide(0)
+        //Update active slide
+        mySwiper.updateActiveSlide(0)
 
-      //Hide loader
-      $('.preloader').removeClass('visible')
-    },1000)
+        //Hide loader
+        $('.preloader').removeClass('visible')
+      },1000)
 
-    slideNumber++;
+      slideNumber++;
   }
 }
 
+// create slides at init phase
+// #TODO read data from storage
+function createMessageSlides(){
+  $('.swiper-wrapper').append(getSlide({title: 'slide 1', style: 'red'}));
+  $('.swiper-wrapper').append(getSlide({title: 'slide 2', style: 'blue'}));
+}
+
+// set up messages slides swiper 
 var messages = {
   initialize : function(){
     createMessageSlides();
+    createMessageSwiper();
   }
 }
