969ee817e3dd3edceab774fdf9f4c0bbfa77f7df
Samurais <hailiang.hl.wang@gmail.com>
2014-09-10 19:20:24 +0800

    #170 add a switcher for online and offline shifting

--- www/js/app/service/agent.js
+++ www/js/app/service/agent.js
@@ -5,26 +5,41 @@ define(function(require, exports, module) {
 	var util = require('app/util');
 	var config = require('app/config');
 	var mapNoty;
+	var netwatch = {};
+	_.extend(netwatch, Backbone.Events);
+
+	netwatch.on('online2offline', function(){
+		mapNoty = noty({text: '无网络服务.',
+							layout: 'center',
+							timeout: 2000,
+							type: 'information'});
+	});
+
+	netwatch.on('offline2online', function(){
+		mapNoty.close();
+		mapNoty = null;
+	});
 
 	// start agent
 	function _start(){
-		console.log('AGENT IS STARTED.')
+		var hasNetwork;
 		setInterval(function(){
 			util.getNetwork().then(function(networkType){
-				// show the warnning 
-				if(mapNoty){
-					mapNoty.close();
-					mapNoty = null;
+				if(typeof(hasNetwork) == 'undefined'){
+					hasNetwork = true;
+				}else if(!hasNetwork){
+					netwatch.trigger('offline2online');
+					hasNetwork = true;
 				}
 			}, function(err){
-				if(!mapNoty){
-					mapNoty = noty({text: '无网络服务.',
-									layout: 'center',
-									timeout: 2000,
-									type: 'information'});
+				if(typeof(hasNetwork) == 'undefined'){
+					hasNetwork = false;
+				}else if(hasNetwork){
+					netwatch.trigger('online2offline');
+					hasNetwork = false;
 				}
 			});
-		},5000);
+		},3000);
 	}
 
 	return {
