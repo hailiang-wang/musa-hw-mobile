89b2a7066b600ce7f922fb0a42269341bfc26bb7
Samurais <hailiang.hl.wang@gmail.com>
2014-09-14 09:12:23 +0800

    #189 tune sign in and sign out btns

--- www/js/app/config.js
+++ www/js/app/config.js
@@ -5,7 +5,7 @@ define({
     pushAppRoute: 'hwcafe.mybluemix.net',
     pushAppSecret : '049180170bdc55b6428f65f96f80518b37296000',
     debug: true,
-    weinre: 'http://192.168.1.103:9088/target/target-script-min.js#musa',
+    weinre: 'http://192.168.9.232:9088/target/target-script-min.js#musa',
     myPremise:'HelloWorldCafe',
     premises : {
     	HelloWorldCafe: {
