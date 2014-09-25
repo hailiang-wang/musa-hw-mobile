d7be44df39aa946858fc5695c394be7b2e7613b5
Samurais <hailiang.hl.wang@gmail.com>
2014-09-25 15:30:49 +0800

    #131 fix names in mobile app

--- www/js/app/config.js
+++ www/js/app/config.js
@@ -5,7 +5,7 @@ define({
     pushAppId : 'a87432fd-2d2e-43d4-91c9-0832af4d4aec',
     pushAppRoute: 'hwcafe.mybluemix.net',
     pushAppSecret : '049180170bdc55b6428f65f96f80518b37296000',
-    debug: false,
+    debug: true,
     weinre: 'http://192.168.2.101:9088/target/target-script-min.js#musa',
     myPremise:'HelloWorldCafe',
     premises : {
