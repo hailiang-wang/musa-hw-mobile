2e41d9d7869efc388c41000af3a662c0944c75f7
Samurais <hailiang.hl.wang@gmail.com>
2014-10-07 19:47:40 +0800

    #15 add listview with search box for people page

--- www/js/app/config.js
+++ www/js/app/config.js
@@ -7,8 +7,7 @@ define({
     pushAppSecret : '049180170bdc55b6428f65f96f80518b37296000',
     // set output for console log 
     // https://code.google.com/p/console-js 
-    console : 'debug',
-    weinreDebug: true,
-    weinreServer: 'http://192.168.2.102:9088/target/target-script-min.js#musa',
-    myPremise:'HelloWorldCafe' 
+    console : 'warn',
+    weinreDebug: false,
+    weinreServer: 'http://192.168.1.106:9088/target/target-script-min.js#musa'
 });
