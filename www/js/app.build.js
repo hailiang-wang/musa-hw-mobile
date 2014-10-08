71743266f491628c9f6f1ea907a53454cd3ce28b
Samurais <hailiang.hl.wang@gmail.com>
2014-10-08 08:28:35 +0800

    #21 optimize css files in standard way

--- www/js/app.build.js
+++ www/js/app.build.js
@@ -19,5 +19,18 @@
         {
             name: "app"
         }
-    ]
+    ],
+    //Allow CSS optimizations. Allowed values:
+    //- "standard": @import inlining and removal of comments, unnecessary
+    //whitespace and line returns.
+    //Removing line returns may have problems in IE, depending on the type
+    //of CSS.
+    //- "standard.keepLines": like "standard" but keeps line returns.
+    //- "none": skip CSS optimizations.
+    //- "standard.keepComments": keeps the file comments, but removes line
+    //returns.  (r.js 1.0.8+)
+    //- "standard.keepComments.keepLines": keeps the file comments and line
+    //returns. (r.js 1.0.8+)
+    //- "standard.keepWhitespace": like "standard" but keeps unnecessary whitespace.
+    optimizeCss: "standard"
 })
