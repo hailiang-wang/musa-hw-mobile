2537a1cd515427ee86da26130348d04ecbdfe7d2
Samurais <hailiang.hl.wang@gmail.com>
2014-10-14 10:24:09 +0800

    #39 parse config.xml just once

--- www/js/app/config.js
+++ www/js/app/config.js
@@ -1,38 +1,44 @@
 define(['jquery'], function($) {
     var configJson = {};
-    var d1 = new Date().getTime();
-    $.ajax({
-        type: 'GET',
-        url: 'config.xml',
-        async: false,
-        success:function(data){
-            var xmlDoc = $.parseXML(data);
-            var $xml = $(xmlDoc);
-            $xml.find('lotuslamp').each(function(index) {
-                var v = {};
-                $.each(this.attributes, function(i, attrib){
-                    switch(attrib.name){
-                        case 'name':
-                            v['name'] = attrib.value;
-                            break;
-                        case 'value':
-                            v['value'] = attrib.value;
-                            break;
-                        default:
-                            break;
-                    }
+    var configSessionKey = 'lotuslamp-config';
+    var configSessionValue = window.sessionStorage.getItem(configSessionKey);
+    
+    if(configSessionValue){
+        configJson = JSON.parse(configSessionValue);
+    }else{
+        // everytime require(config) from config.xml, it takes 0.017 second, a bit slow.
+        // also export the config into session storage for speed up access
+        // config parameters
+        $.ajax({
+            type: 'GET',
+            url: 'config.xml',
+            async: false,
+            success:function(data){
+                var xmlDoc = $.parseXML(data);
+                var $xml = $(xmlDoc);
+                $xml.find('lotuslamp').each(function(index) {
+                    var v = {};
+                    $.each(this.attributes, function(i, attrib){
+                        switch(attrib.name){
+                            case 'name':
+                                v['name'] = attrib.value;
+                                break;
+                            case 'value':
+                                v['value'] = attrib.value;
+                                break;
+                            default:
+                                break;
+                        }
+                    });
+                    configJson[v.name] = v.value;
                 });
-                configJson[v.name] = v.value;
-            });
-        },
-        error:function(xhr, statusCode, errorThrown){
-            console.error(errorThrown);
-        }
-    });
-    // TODO in this way, everytime require(config) takes 0.017 second, a bit slow.
-    // also export the config into session storage for speed up access
-    // config parameters
-    // window.sessionStorage.setItem('lotuslamp-config', JSON.stringify(configJson));
+            },
+            error:function(xhr, statusCode, errorThrown){
+                console.error(errorThrown);
+            }
+        });
+        window.sessionStorage.setItem(configSessionKey, JSON.stringify(configJson));
+    }
 
     return configJson;
 })
