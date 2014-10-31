d6223526d1807c56b03ac0f64a6bd51965db7bfa
Samurais <hailiang.hl.wang@gmail.com>
2014-10-31 22:00:12 +0800

    #40 upgrade to cordova@3.6.3-0.2.13

--- .cordova/hooks/before_prepare/copy-ios-native.js
+++ .cordova/hooks/before_prepare/copy-ios-native.js
@@ -11,14 +11,10 @@ if (!_NMR) {
   return;
 }
 
-var cordova_util = require(path.join(_NMR, 'cordova/src/util'));
-var platforms = require(path.join(_NMR, 'cordova/platforms'));
+var cordova_util = require('cordova-lib/src/cordova/util');
 var projRoot = cordova_util.isCordova(process.cwd());
 var projXml = cordova_util.projectConfig(projRoot);
-var ConfigParser = cordova_util.config_parser
-if(undefined === ConfigParser) {
-    ConfigParser = require(path.join(_NMR, 'cordova/src/ConfigParser'));
-}
+var ConfigParser = require('cordova-lib/src/ConfigParser/ConfigParser');
 var projConfig = new ConfigParser(projXml);
 var projName = projConfig.name();
 
